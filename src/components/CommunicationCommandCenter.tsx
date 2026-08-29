424faf62898b8ba40622a3fbba851d9d5699d9fe
Eugene999B <kuamieugene321@gmail.com>
2026-08-29 16:02:20 +0000

    fix(communications): restore high-contrast bulk action buttons

--- src/components/CommunicationCommandCenter.tsx
+++ src/components/CommunicationCommandCenter.tsx
@@ -2,4 +2,27 @@
 
 import CommunicationCommandCenterV2 from "./CommunicationCommandCenterV2";
 
-export default CommunicationCommandCenterV2;
+type Props = React.ComponentProps<typeof CommunicationCommandCenterV2>;
+
+export default function CommunicationCommandCenter(props: Props) {
+  return (
+    <>
+      <style jsx global>{`
+        .sukuu-communication button[type="submit"] {
+          background-color: #0f172a !important;
+          color: #ffffff !important;
+          border-color: #0f172a !important;
+        }
+        .sukuu-communication button[type="submit"]:hover:not(:disabled) {
+          background-color: #1e293b !important;
+        }
+        .sukuu-communication button[type="submit"]:disabled {
+          opacity: 0.5;
+        }
+      `}</style>
+      <div className="sukuu-communication">
+        <CommunicationCommandCenterV2 {...props} />
+      </div>
+    </>
+  );
+}
