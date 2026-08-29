24a36a5f04dc457dd3b44e26fbb8c209c1b6a377
Eugene999B <kuamieugene321@gmail.com>
2026-08-29 00:10:33 +0000

    feat: add platform visitor inbox page

---
+++ src/app/platform/inbox/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from "@/components/AppShell";
+import { PublicLeadInbox } from "@/components/PublicLeadInbox";
+export default function InboxPage(){return <AppShell universe="platform" title="Visitor Inbox" subtitle="Messages from people who found SukuuNova on the public site, ready for a human follow-up."><PublicLeadInbox /></AppShell>;}
