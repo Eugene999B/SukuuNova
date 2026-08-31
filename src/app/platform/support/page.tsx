9b7e84d702e21a4e01c610402ff2f6cb89989834
Eugene999B <kuamieugene321@gmail.com>
2026-08-31 03:56:18 +0000

    feat(platform): route support to real support console

--- src/app/platform/support/page.tsx
+++ src/app/platform/support/page.tsx
@@ -1,2 +1,2 @@
-import PlatformAdminWorkspace from "@/components/PlatformAdminWorkspace";
-export default function SupportPage(){return <PlatformAdminWorkspace section="support"/>;}
+import SupportConsole from "./SupportConsole";
+export default function SupportPage(){return <SupportConsole/>;}
