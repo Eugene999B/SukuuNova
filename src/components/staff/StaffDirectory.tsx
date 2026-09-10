7ccee656d33858fdea5d0302039638e218c2e129
Eugene999B <kuamieugene321@gmail.com>
2026-09-10 09:14:19 +0000

    style: load staff profile link styles

--- src/components/staff/StaffDirectory.tsx
+++ src/components/staff/StaffDirectory.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useMemo, useState } from "react";
 import { ArrowRight } from "lucide-react";
+import "./staff-profile-link.css";
 
 type StaffRow = {
   id: string;
