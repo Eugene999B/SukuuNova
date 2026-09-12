79022382c2ee84ee4d4dc8f7a74a0f193cd531fa
Eugene999B <kuamieugene321@gmail.com>
2026-09-12 14:15:55 +0000

    Update session variety floor for 18 flagship worlds

--- tests/arcade-v5-remix.test.ts
+++ tests/arcade-v5-remix.test.ts
@@ -28,7 +28,7 @@ describe("Arcade V5 session remix", () => {
   });
 
   it("exposes a combinatorial floor above one million before content variation", () => {
-    expect(arcadeSessionVarietySpace()).toBe(1_305_600);
+    expect(arcadeSessionVarietySpace()).toBe(1_382_400);
   });
 });
 
